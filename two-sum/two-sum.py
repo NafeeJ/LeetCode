657b0550657e249941b1afd7105778e495c95ab6
Nafee Jan <nafeejan2@gmail.com>
2021-10-20 17:45:29 -0400

    Time: 75 ms (59.58%), Space: 15.3 MB (55.51%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,6 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i, i_val in enumerate(nums):
-            for j, j_val in enumerate(nums):
-                if ((i != j) and (i_val + j_val == target)):
-                    return [i, j]
+        cache = {}
+        # Given that n = target - m, where n and m are any two numbers:
+        for i, n in enumerate(nums):
+            m = target - n
+            if m in cache:
+                return [cache[m], i]
+            else:
+                cache[n] = i
+        return null
