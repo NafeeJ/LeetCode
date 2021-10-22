34fdd6856e9be3b413811f1ac0887ba674ffc50d
Nafee Jan <nafeejan2@gmail.com>
2021-10-22 14:43:38 -0400

    Time: 362 ms (19.21%), Space: 22.4 MB (16.05%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.py
+++ product-of-array-except-self/product-of-array-except-self.py
@@ -4,11 +4,9 @@ class Solution:
         left = [1] * num_len
         right = [1] * num_len
         
-        left.append(1)
         for i in range(1, num_len):
             left[i] = left[i - 1] * nums[i - 1]
         
-        right[num_len - 1] = 1
         for i in range(num_len)[-2::-1]:
             right[i] = right[i + 1] * nums[i + 1]
         
